6f80dd8805faf25a304aabe8ed7d484c57cc73ac
Yuri <brunaodolavajato15@gmail.com>
2025-04-30 20:21:30 -0300

    Create import.hx

---
+++ source/import.hx
@@ -0,0 +1,17 @@
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.group.FlxGroup;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.FlxObject;
+import flixel.FlxCamera;
+
+import sys.FileSystem;
+import sys.io.File;
+import haxe.Json;
+
+import utils.FileUtil;
+import utils.Logger;
+import utils.ModManager;
