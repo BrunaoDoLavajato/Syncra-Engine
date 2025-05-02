677fd9e6a2603368b1447a45ab566dd00b36f5f9
Yuri <brunaodolavajato15@gmail.com>
2025-05-02 20:51:56 -0300

    Update FlxGridOverlay.hx

--- source/utils/FlxGridOverlay.hx
+++ source/utils/FlxGridOverlay.hx
@@ -1,31 +1,33 @@
 package utils;
 
 import flixel.FlxSprite;
-import flixel.group.FlxTypedGroup;
+import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
 
 class FlxGridOverlay {
+    /**
+     * Creates a simple checkerboard overlay by generating a FlxGroup of solids.
+     */
     public static function create(
         cellWidth:Int,
         cellHeight:Int,
         width:Int,
-        height:Int,
-        verticalLines:Bool = true,
-        ?color1:FlxColor,
-        ?color2:FlxColor
-    ):FlxTypedGroup<FlxSprite> {
-        if (color1 == null) color1 = FlxColor.GRAY;
-        if (color2 == null) color2 = FlxColor.fromRGB(64, 64, 64);
-
-        var group = new FlxTypedGroup<FlxSprite>();
+        height:Int
+    ):FlxGroup {
+        var group = new FlxGroup();
         var cols = Math.ceil(width / cellWidth);
         var rows = Math.ceil(height / cellHeight);
+
+        // two alternating colors
+        var c1 = FlxColor.GRAY;
+        var c2 = FlxColor.fromRGB(64, 64, 64);
 
         for (y in 0...rows) {
             for (x in 0...cols) {
                 var block = new FlxSprite(x * cellWidth, y * cellHeight);
-                var color = ((x + y) % 2 == 0) ? color1 : color2;
+                var color = ((x + y) % 2 == 0) ? c1 : c2;
                 block.makeGraphic(cellWidth, cellHeight, color);
+                block.alpha = 0.2;
                 group.add(block);
             }
         }
