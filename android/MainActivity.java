fe291b35d0b6bc21dfb26bd985109e432d6a98fc
Yuri <brunaodolavajato15@gmail.com>
2025-05-02 11:35:05 -0300

    I hate github

---
+++ android/MainActivity.java
@@ -0,0 +1,12 @@
+package org.haxe;
+
+import android.os.Bundle;
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Additional setup if needed
+    }
+}
