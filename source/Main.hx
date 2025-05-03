6b979f35a266e21c0bbf24a10efc8636df62dd1a
Yuri <brunaodolavajato15@gmail.com>
2025-05-02 21:55:32 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,14 +1,12 @@
 package;
 
-import openfl.display.Sprite;
 import flixel.FlxGame;
+import openfl.display.Sprite;
 import states.MainMenuState;
 
-@:keep
 class Main extends Sprite {
     public function new() {
         super();
-        // width, height, initial state
-        addChild(new FlxGame(1280, 720, MainMenuState));
+        addChild(new FlxGame(1280, 730, MainMenuState, 1, 60, 60, true));
     }
 }
