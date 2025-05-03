fd63d608ba66cc2db12a9cae30f066df79f3b5ad
Yuri <brunaodolavajato15@gmail.com>
2025-05-03 13:11:35 -0300

    Update ModsMenuState.hx

--- source/states/ModsMenuState.hx
+++ source/states/ModsMenuState.hx
@@ -3,15 +3,12 @@ package states;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.group.FlxTypedGroup;
-import flixel.group.FlxGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import flixel.graphics.frames.FlxAtlasFrames;
-import haxe.Json;
+import openfl.utils.Assets;
 import sys.io.File;
 import sys.FileSystem;
-import utils.FlxGridOverlay;
+import haxe.Json;
 
 class ModsMenuState extends FlxState {
     override public function create():Void {
@@ -21,20 +18,18 @@ class ModsMenuState extends FlxState {
         var bg = new FlxSprite().loadGraphic("assets/images/menus_assets/menuBGMagenta.png");
         add(bg);
 
-        // Checkerboard overlay
-        var checkerboard:FlxGroup = FlxGridOverlay.create(40, 40, FlxG.width, FlxG.height);
-        for (sprite in checkerboard.members) {
-            var casted:FlxSprite = cast sprite;
-            if (casted != null) casted.alpha = 0.2;
-        }
-        add(checkerboard);
+        // Semi-transparent overlay
+        var overlay = new FlxSprite();
+        overlay.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+        overlay.alpha = 0.2;
+        add(overlay);
 
-        // Gray background behind icon
+        // Gray box for icon background
         var grayBg = new FlxSprite(100, 100);
         grayBg.makeGraphic(170, 170, FlxColor.GRAY);
         add(grayBg);
 
-        // Load Mod Info
+        // Load mod info
         var modFolder = "mods/YourModFolder/";
         var infoPath = modFolder + "Mod-Info.json";
 
@@ -46,36 +41,25 @@ class ModsMenuState extends FlxState {
         var infoRaw = File.getContent(infoPath);
         var modInfo:Dynamic = Json.parse(infoRaw);
 
-        // Mod Icon
-        var modIcon = new FlxSprite(110, 110).loadGraphic(modFolder + "Mod-Icon.png");
+        // Load Mod Icon
+        var modIcon = new FlxSprite(110, 110);
+        modIcon.loadGraphic(modFolder + "Mod-Icon.png");
         modIcon.setGraphicSize(150, 150);
         modIcon.updateHitbox();
         add(modIcon);
 
-        // Mod Name (using alphabet.png)
-        var nameText = new FlxTypedGroup<FlxSprite>();
-        var name:String = Std.string(modInfo.name);
-        var xStart = 300;
-        for (i in 0...name.length) {
-            var letter = name.charAt(i).toLowerCase();
-            if (letter == " ") continue;
-
-            var charSprite = new FlxSprite(xStart + i * 30);
-            charSprite.frames = FlxAtlasFrames.fromSparrow("assets/images/alphabet.png", "assets/images/alphabet.xml");
-            charSprite.animation.addByPrefix("idle", letter, 24, false);
-            charSprite.animation.play("idle");
-            charSprite.y = 120;
-            nameText.add(charSprite);
-        }
+        // Display Mod Name
+        var nameText = new FlxText(300, 120, 400, Std.string(modInfo.name));
+        nameText.setFormat("assets/fonts/vcr.ttf", 24, FlxColor.WHITE);
         add(nameText);
 
-        // Description
-        var descriptionText = new FlxText(300, 170, 400, Std.string(modInfo.description));
-        descriptionText.setFormat("assets/fonts/vcr.ttf", 16, FlxColor.WHITE);
+        // Display Description
+        var descriptionText = new FlxText(300, 160, 400, Std.string(modInfo.description));
+        descriptionText.setFormat("assets/fonts/vcr.ttf", 16, FlxColor.LTGRAY);
         add(descriptionText);
 
-        // Save to mod list
-        updateModList(name);
+        // Update mod list based on platform
+        updateModList(Std.string(modInfo.name));
     }
 
     function updateModList(modName:String):Void {
