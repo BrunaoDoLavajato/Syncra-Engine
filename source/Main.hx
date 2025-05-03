b90fbf73b60b41bb08dd0d72605c7ae590aa311a
Yuri <brunaodolavajato15@gmail.com>
2025-05-03 06:34:31 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -7,6 +7,6 @@ import states.MainMenuState;
 class Main extends Sprite {
     public function new() {
         super();
-        addChild(new FlxGame(1280, 730, MainMenuState, 1, 60, 60, true));
+        addChild(new FlxGame(1280, 720, MainMenuState, 1, 60, 60, true));
     }
 }
